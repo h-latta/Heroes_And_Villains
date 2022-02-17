369243add47352c3ed3bce0af5335182be09ebff
h-latta <h.latta@yahoo.com>
2022-02-17 11:08:01 -0600

    Created Models and Serializers for Supers and Super Types, Updated Settings to Reflect App Additions

---
+++ super_types/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import SuperTypes
+
+class SuperTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuperTypes
+        fields = ['type']
