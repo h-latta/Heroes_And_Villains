b9eba71970f71f455312fa85d3a5ed2f1fc02f86
h-latta <h.latta@yahoo.com>
2022-02-17 13:57:53 -0600

    Created GET, PUT, DELETE Function for Supers by ID in Views File, Updated URLs to Allow Command Execution

--- supers/urls.py
+++ supers/urls.py
@@ -3,5 +3,6 @@ from django import views
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_supers)
+    path('', views.get_all_supers),
+    path('<int:pk>/', views.get_supers_id)
 ]
--- supers/views.py
+++ supers/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Supers
 from .serializers import SuperSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -17,3 +18,18 @@ def get_all_supers(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def get_supers_id(request, pk):
+    superhero_id = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(superhero_id)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(superhero_id, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        superhero_id.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
