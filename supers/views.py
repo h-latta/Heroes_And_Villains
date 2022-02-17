fbe4827f0f3634810f2b68ad0f6009825ee09332
h-latta <h.latta@yahoo.com>
2022-02-17 13:45:07 -0600

    Created POST Function for Supers Views File

--- supers/views.py
+++ supers/views.py
@@ -2,12 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Supers
 from .serializers import SuperSerializer
+from rest_framework import status
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_all_supers(request):
     if request.method == 'GET':
         superhero = Supers.objects.all()
         serializer = SuperSerializer(superhero, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
