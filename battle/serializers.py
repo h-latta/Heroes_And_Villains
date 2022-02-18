591acdc9561b9fd4c787ef419dd85a1c16823026
h-latta <h.latta@yahoo.com>
2022-02-18 13:51:59 -0600

    Created Battle Serializer File, Created URLs Files for Battle and SuperTypes Apps

---
+++ battle/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from models import Battle
+
+class BattleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Battle
+        fields = ['id', 'super_one', 'super_one_id', 'super_two', 'super_two_id', 'battle_date']
+        depth = 1
+    super_one_id = serializers.IntegerField(write_only=True)
+    super_two_id = serializers.IntegerField(write_only=True)
