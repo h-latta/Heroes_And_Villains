8606fea52ac74a0085610f2b3419fc09c5da25cf
h-latta <h.latta@yahoo.com>
2022-02-18 10:34:12 -0600

    Created Battle Model and Added Model to Admin Site

--- battle/admin.py
+++ battle/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from battle.models import Battle
+
 # Register your models here.
+
+admin.site.register(Battle)
--- battle/models.py
+++ battle/models.py
@@ -1,3 +1,10 @@
+from datetime import datetime
 from django.db import models
+from supers.models import Supers
 
 # Create your models here.
+
+class Battle(models.Model):
+    super_one = models.ForeignKey(Supers, on_delete=models.CASCADE)
+    super_two = models.ForeignKey(Supers, on_delete=models.CASCADE)
+    battle_date = datetime
