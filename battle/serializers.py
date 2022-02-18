1e9dc4c8ecfe736f164fbd4a59ec5826ad8d8da8
h-latta <h.latta@yahoo.com>
2022-02-18 14:11:25 -0600

    Updated URLs File to Allow HTTP Function for Battle App, Changed Battle Serializer Depth to Show SuperTypes for Supers

--- battle/serializers.py
+++ battle/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from models import Battle
+from .models import Battle
 
 class BattleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Battle
         fields = ['id', 'super_one', 'super_one_id', 'super_two', 'super_two_id', 'battle_date']
-        depth = 1
+        depth = 2
     super_one_id = serializers.IntegerField(write_only=True)
     super_two_id = serializers.IntegerField(write_only=True)
